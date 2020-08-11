d1ef0409534703684448995c8998c7197aa4f3a2
Devon Reihl <devon.reihl@gmail.com>
2020-08-11 14:23:14 -0700

    reloading files to gitHub

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
- /* 
+/* 
  * Technical requirements:
  * 
  * Your app should include a render() function, that regenerates the view each time the store is updated. 
@@ -18,11 +18,27 @@
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+function generateQuiz(){
+  //this function with generate the wireframe for main question page
+  return `<div>
+      <p>Question</p>
+
+      <form action="/action_page.php" method="get">
+        <input type="radio" name="true" value="true">
+        <label for="true">True</label><br>
+        <input type="radio" name="false" value="false">
+        <label for="false">False</label><br>
+
+        <button type="submit" value="submit">Submit</button>
+        <button type="reset" value="reset">Reset</button><br>
+        <button type="submit" value="next">Next Question</button>
+
+      </form>
+    </div>`;
+}
 
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
 /********** EVENT HANDLER FUNCTIONS **********/
-
-// Truth about Protein quiz
--- store.js
+++ store.js
@@ -38,18 +38,33 @@
 
 // // These functions handle events (submit, click, etc)
 
-// Does all protein come from plants? T  wrong answer links to information
-// Proteins are long chains of triglycerides? F they are long chains of amino acids
-// the Human body makes all 20 essential amino acids? F human body only makes 11
-// There are 9 essential amino acids to make up a complete protein? T  wrong answer says body doesn't make those nine.
-// All plants are missing amino acids to be a proper protein? F They have moderate amounts of all 20 essential amino acids.
-// The best way to get your proper amount of protein is to eat fish? F no it is best to eat a varity of whole foods.
 
 const STORE = [
   {question: 'Does all protein come from plants?', answer: true, explanation: ''},
   {question: 'Proteins are long chains of triglycerides?', answer: false, explanation: 'Proteins are long chains of amino acids.' },
   {question: 'The Human body makes all 20 essential amino acids?', answer: false, explanation: 'In healthy adults, 9 of the 20 amino acids are considered essential (also called indispensable). Your body cannot make them, so they must come from your diet'},
   {question: 'There are 9 essential amino acids to make up a complete protein?', answer: true, explanation: 'A food (or combination of foods) is considered to be a complete protein source if it provides “enough” of each of the nine essential amino acids'},
-  {question: 'All plants are missing amino acids to be a proper protein?', answer: false, explanation: 'The widely held belief that plant-based proteins are “incomplete” or “missing” amino acids traces back to a 1971 book'},
   {question: 'The best way to get your proper amount of protein is to eat fish?', answer: false, explanation: 'Eat a variety of whole foods.'},
 ];
+
+//How many essential amino acids are there? Choices 8, 9, 10, 11 correct 9
+// How many amino acids does the human body make naturally? 9, 11, 20, 15 correct 11
+// Where does all protein originate? 'plants' 'dairy' 'animals' 'fungi'
+// Protein is long chains of what? choices, ‘amino acids’, ‘fatty acids’, ‘ascorbic acid’, ‘hydrochloric acid’ correct amino acids
+// The best way to get a “complete protein” is to consume? choices: ‘nuts’, ‘meat’, ‘a variety of foods’, ‘protein powders’
+
+
+
+
+
+2:02
+answer: variety of foods
+
+
+
+
+
+2:01
+choices: ‘nuts’, ‘meat’, ‘a variety of foods’, ‘protein powders’
+2:02
+answer: variety of foods
