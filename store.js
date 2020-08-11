b624d52385a3ee20ed27c94ff1ab0f987aa39fe6
Devon Reihl <devon.reihl@gmail.com>
2020-08-11 15:57:33 -0700

    updated STORE

--- store.js
+++ store.js
@@ -1,43 +1,72 @@
 'use strict';
 
-/**
- * Example store structure
- */
-// const store = {
-//     // 5 or more questions are required
-//     questions: [
-//       {
-//         question: 'What color is broccoli?',
-//         answers: [
-//           'red',
-//           'orange',
-//           'pink',
-//           'green'
-//         ],
-//         correctAnswer: 'green'
-//       },
-//       {
-//         question: 'What is the current year?',
-//         answers: [
-//           '1970',
-//           '2015',
-//           '2019',
-//           '2005'
-//         ],
-//         correctAnswer: '2019'
-//       }
-//     ],
-//     quizStarted: false,
-//     questionNumber: 0,
-//     score: 0
-//   };
-  
-//   /*
+const store = {
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'Where does all protein originate?',
+      answers: [
+        'dairy',
+        'animals',
+        'plants',
+        'the moon'
+      ],
+      correctAnswer: 'plants'
+    },
+    {
+      question: 'Protein is long chains of what?',
+      answers: [
+        'ascorbic acid',
+        'triglycerides',
+        'hydrochloirc acid',
+        'amino acids'
+      ],
+      correctAnswer: 'amino acids'
+    },
+    {
+      question: 'How make amino acids are used to build proteins?',
+      answers: [
+        '9',
+        '20',
+        '11',
+        '8'
+      ],
+      correctAnswer: '20'
+    },
+    {
+      question: 'What makes an amino acid "essential"?',
+      answers: [
+        'They are made by humans only',
+        'They are used to build muscles',
+        'They are needed to create proteins',
+        'They are only found in supplements'
+      ],
+      correctAnswer: 'They are needed to create proteins'
+    },
+    {
+      question: 'How many amino acids does the human body make naturally?',
+      answers: [
+        '8',
+        '11',
+        '20',
+        '15'
+      ],
+      correctAnswer: '11'
+    },
+    {
+      question: 'How many essential amino acids are there?',
+      answers: [
+        '8',
+        '9',
+        '10',
+        '11'
+      ],
+      correctAnswer: '9'
+    }
 
 
-
-//How many essential amino acids are there? Choices 8, 9, 10, 11 correct 9
-// How many amino acids does the human body make naturally? 9, 11, 20, 15 correct 11
-// Where does all protein originate? 'plants' 'dairy' 'animals' 'fungi'
-// Protein is long chains of what? choices, ‘amino acids’, ‘fatty acids’, ‘ascorbic acid’, ‘hydrochloric acid’ correct amino acids
-// The best way to get a “complete protein” is to consume? choices: ‘nuts’, ‘meat’, ‘A variety of foods’, ‘protein powders’
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
