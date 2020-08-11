46308d4b7cf76558ac4326525f1a7089159b5aef
Devon Reihl <devon.reihl@gmail.com>
2020-08-11 15:23:38 -0700

    changed html and app.js

--- app.js
+++ app.js
@@ -2,39 +2,77 @@
 
 
 /* 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
+ *User experience requirements
+The following requirements cover what the app must do, from the user's perspective.
+
+The starting screen should have a button that users can click to start the quiz.
+Users should be prompted through a series of at least 5 multiple choice questions that they can answer.
+Users should be asked questions 1 after the other.
+Users should only be prompted with 1 question at a time.
+Users should not be able to skip questions.
+Users should also be able to see which question they're on (for instance, "7 out of 10") and their current score ("5 correct, 2 incorrect").
+Upon submitting an answer, users should:
+receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
+be moved onto the next question (or interact with an element to move on).
+Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
+Users should be able to start a new quiz.
+Technical requirements
+Your quiz app must:
+
+Include a render() function, that conditionally regenerates the view each time the store is updated.
+Include single-purpose template generation functions.
+Include event handler functions.
+NOT add additional HTML elements to the boilerplate code's index.html file (you may add attributes, e.g., classes and ids, to the existing HTML elements, or link stylesheets or additional scripts if necessary).
+Render answer choices in a <form>.
+Use semantic HTML, along with CSS and jQuery.
+Follow a11y best practices.
+Be fully usable by keyboard (which will be easy enough if you start with a form).
+Use responsive design.
+Refer back to the previous checkpoints on responsive design and forms for any help with the HTML/CSS materials.
  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
  * 
  */
+// create out questions and answers here reference in Html
+
+// // These functions handle events (submit, click, etc)
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
 function generateQuiz(){
   //this function with generate the wireframe for main question page
-  return `<div>
-      <p>Question</p>
+  return `<div class="two">
+      
 
-      <form action="/action_page.php" method="get">
-        <input type="radio" name="true" value="true">
-        <label for="true">True</label><br>
-        <input type="radio" name="false" value="false">
-        <label for="false">False</label><br>
+  <form action="/action_page.php" method="get">
+    <p>QUESTION</p> <!-- fill QUESTION with jQuery -->
+    
+    <div>
+      <input type="radio" id="option1"
+       name="contact" value="text">
+      <label for="option1">ANSWER</label> <!-- fill ANSWER with jQuery to access answers -->
 
-        <button type="submit" value="submit">Submit</button>
-        <button type="reset" value="reset">Reset</button><br>
-        <button type="submit" value="next">Next Question</button>
+      <input type="radio" id="option2"
+       name="contact" value="text">
+      <label for="option2">ANSWER</label>
+  
+      <input type="radio" id="option3"
+       name="contact" value="text">
+      <label for="option3">ANSWER</label>
 
-      </form>
-    </div>`;
+      <input type="radio" id="option4"
+       name="contact" value="text">
+      <label for="option4">ANSWER</label>
+  
+      <input type="radio" id="option5"
+       name="contact" value="text">
+      <label for="option5">ANSWER</label>
+    </div>
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</div>`;
 }
 
 /********** RENDER FUNCTION(S) **********/
--- store.js
+++ store.js
@@ -34,37 +34,10 @@
   
 //   /*
 
-// create out questions and answers here reference in Html
 
-// // These functions handle events (submit, click, etc)
-
-
-const STORE = [
-  {question: 'Does all protein come from plants?', answer: true, explanation: ''},
-  {question: 'Proteins are long chains of triglycerides?', answer: false, explanation: 'Proteins are long chains of amino acids.' },
-  {question: 'The Human body makes all 20 essential amino acids?', answer: false, explanation: 'In healthy adults, 9 of the 20 amino acids are considered essential (also called indispensable). Your body cannot make them, so they must come from your diet'},
-  {question: 'There are 9 essential amino acids to make up a complete protein?', answer: true, explanation: 'A food (or combination of foods) is considered to be a complete protein source if it provides “enough” of each of the nine essential amino acids'},
-  {question: 'The best way to get your proper amount of protein is to eat fish?', answer: false, explanation: 'Eat a variety of whole foods.'},
-];
 
 //How many essential amino acids are there? Choices 8, 9, 10, 11 correct 9
 // How many amino acids does the human body make naturally? 9, 11, 20, 15 correct 11
 // Where does all protein originate? 'plants' 'dairy' 'animals' 'fungi'
 // Protein is long chains of what? choices, ‘amino acids’, ‘fatty acids’, ‘ascorbic acid’, ‘hydrochloric acid’ correct amino acids
-// The best way to get a “complete protein” is to consume? choices: ‘nuts’, ‘meat’, ‘a variety of foods’, ‘protein powders’
-
-
-
-
-
-2:02
-answer: variety of foods
-
-
-
-
-
-2:01
-choices: ‘nuts’, ‘meat’, ‘a variety of foods’, ‘protein powders’
-2:02
-answer: variety of foods
+// The best way to get a “complete protein” is to consume? choices: ‘nuts’, ‘meat’, ‘A variety of foods’, ‘protein powders’
